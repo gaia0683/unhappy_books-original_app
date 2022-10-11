50cd848ccdf1c43c0141d6ff3abc8e4d666defcf
bomute kim <kimbomte1996@icloud.com>
2022-10-11 15:33:39 +0900

    docs READMEを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  resources :books
+  root 'books#index'
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
