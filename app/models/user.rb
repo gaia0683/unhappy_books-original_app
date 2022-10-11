5f157ad270b61f9688514a09c79dd3b9dd814ebf
bomute kim <kimbomte1996@icloud.com>
2022-10-11 13:40:44 +0900

    feat Bookモデルを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  before_validation { email.downcase! }
 end
