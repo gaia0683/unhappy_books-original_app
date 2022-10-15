cba8790750dec7aca71aa5513d768a34969ba38f
bomute kim <kimbomte1996@icloud.com>
2022-10-15 18:40:38 +0900

    feat 本の詳細ページを作成 #10

--- app/controllers/book_searches_controller.rb
+++ app/controllers/book_searches_controller.rb
@@ -3,11 +3,38 @@ class BookSearchesController < ApplicationController
   end
 
   def index
+    @books = []
     if params[:keyword]
-      @book_searches = RakutenWebService::Books::Book.search(title: params[:keyword], booksGenreId: params[:booksGenreId])
+      results = RakutenWebService::Books::Book.search(title: params[:keyword], booksGenreId: params[:booksGenreId],outOfStockFlag: params[:outOfStockFlag])
+
+      results.each do |result|
+        book = Book.new(read(result))
+        @books << book
+      end
     end
   end
 
   def show
+    @book = Book.find_or_initialize_by(isbn: params[:isbn])
+    results = RakutenWebService::Books::Book.search(isbn: @book.isbn)
+    @book = Book.new(read(results.first))
+  end
+
+  private
+  def read(result)
+    title = result["title"]
+    image = result["largeImageUrl"]
+    author = result["author"]
+    publisher_name = result["publisherName"]
+    url = result["itemUrl"]
+    isbn = result["isbn"]
+    {
+      title: title,
+      image: image,
+      author: author,
+      publisher_name: publisher_name,
+      url: url,
+      isbn: isbn
+    }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   get 'users/show' => 'users#show'
-  resources :books
+  resources :books, only: [:index]
   root 'book_searches#new'
-  resources :book_searches, only: [:index, :new, :show]
+  resources :book_searches, only: [:index, :new,:show], param: :isbn
 
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
