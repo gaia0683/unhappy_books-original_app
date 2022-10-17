6707cff6df88689f5284b8b097a82909a691cc71
bomute kim <kimbomte1996@icloud.com>
2022-10-18 08:35:39 +0900

    feat 管理者機能を実装 #18

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :nickname, presence: true, uniqueness: true
   before_validation { email.downcase! }
+  has_many :reviews
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = 'ゲスト'
+      user.nickname = 'ゲスト'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: { registrations: 'users/registrations' }
   get 'users/show' => 'users#show'
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
   resources :books, only: [:index]
   root 'book_searches#new'
   resources :book_searches, only: [:index, :new,:show], param: :isbn
-
+  resources :reviews
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
