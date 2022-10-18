e735f98b7c0411103a283168d3ede0e2a0269d61
bomute kim <kimbomte1996@icloud.com>
2022-10-18 11:40:41 +0900

    feat テーブルのアソシエーションを修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_17_062709) do
+ActiveRecord::Schema.define(version: 2022_10_18_023329) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2022_10_17_062709) do
     t.float "point"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "review_id", null: false
+    t.index ["review_id"], name: "index_categories_on_review_id"
   end
 
   create_table "reviews", force: :cascade do |t|
@@ -59,6 +61,7 @@ ActiveRecord::Schema.define(version: 2022_10_17_062709) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "reviews"
   add_foreign_key "reviews", "books"
   add_foreign_key "reviews", "users"
 end
