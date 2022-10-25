46a04c7cdf85b2ffd122b389c736181fccfe41b3
bomute kim <kimbomte1996@icloud.com>
2022-10-25 13:29:14 +0900

    feat トップページを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  root 'top#index'
   get 'users/show' => 'users#show'
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
-  root 'books#index'
   resources :book_searches, only: [:index,:new,:show], param: :isbn
   resources :books, only: [:index, :show] do
     resources :reviews, except: [:index,:show]
