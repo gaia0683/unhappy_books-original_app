2e7c98ee181dae46ed419bae7ba278f58d49a8c7
bomute kim <kimbomte1996@icloud.com>
2022-10-26 18:33:47 +0900

    test reviewのcreate機能をテスト

--- spec/system/review_spec.rb
+++ spec/system/review_spec.rb
@@ -1,8 +1,40 @@
 require 'rails_helper'
-RSpec.describe 'レビューの作成機能' type: :system do
+RSpec.describe 'レビューの作成機能', type: :system do
   describe 'レビューの作成が成功した時' do
-    context '' do
+    before do
+      FactoryBot.create(:user)
+      visit new_user_session_path
 
+      fill_in 'user_email', with: 'kimu@kimu.com'
+      fill_in 'user_password', with: 'gaia0683'
+
+      click_button 'ログイン'
+    end
+    context 'レビューを新規作成する場合', js: true do
+      it 'レビューが作成できること' do
+        click_link '本を検索'
+
+        fill_in 'keyword', with: '火車'
+
+        click_button '本の題名を検索'
+
+        book_shows = all('.book_show')
+
+        book_shows[0].click
+
+        click_link 'レビューを作成'
+
+        fill_in 'review_title', with: 'aa'
+        fill_in 'review_content', with: 'aa'
+
+        #category = all('.rating_category')
+        #expect(page).to have_selector("img[src$='star-off']")
+
+        #select '後味悪い', from: "#{category[0]}"
+        binding.irb
+        find('#rating_point_0','img[alt="3"]').click
+        click_button '登録する'
+      end
     end
   end
 end
