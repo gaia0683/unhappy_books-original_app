dead5abde39d841982aa230dc3abe45294dd79b6
bomute kim <kimbomte1996@icloud.com>
2022-10-26 12:06:10 +0900

    feat 管理者権限を持つゲストログインを実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,13 @@ class User < ApplicationRecord
       user.nickname = 'ゲスト'
     end
   end
+
+  def self.admin_guest
+    find_or_create_by!(email: 'admin_guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = 'ゲスト管理者'
+      user.nickname = 'ゲスト管理者'
+      user.admin = true
+    end
+  end
 end
