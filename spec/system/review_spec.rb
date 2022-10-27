c21b6b980e787c1d089b45c78df6f29bb284b28c
bomute kim <kimbomte1996@icloud.com>
2022-10-27 22:51:54 +0900

    test reviewのテストを実装,userの足りないテストを追記

--- spec/system/review_spec.rb
+++ spec/system/review_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'レビューの作成機能', type: :system do
 
       click_button 'ログイン'
     end
-    context 'レビューを新規作成する場合', js: true do
+    context 'レビューを新規作成する場合' do
       it 'レビューが作成できること' do
         click_link '本を検索'
 
@@ -25,9 +25,148 @@ RSpec.describe 'レビューの作成機能', type: :system do
         click_link 'レビューを作成'
 
         fill_in 'review_title', with: 'aa'
-        fill_in 'review_content', with: 'aa'
+        fill_in 'review_content', with: 'ab'
 
+        click_button '登録する'
 
+        expect(page).to have_selector 'p', text: 'kk'
+        expect(page).to have_selector 'h3', text: 'aa'
+        expect(page).to have_selector 'p', text: 'ab'
+      end
+      it '本の詳細ページにレビューが表示されていること' do
+        click_link '本を検索'
+
+        fill_in 'keyword', with: '火車'
+
+        click_button '本の題名を検索'
+
+        book_shows = all('.book_show')
+
+        book_shows[0].click
+
+        click_link 'レビューを作成'
+
+        fill_in 'review_title', with: 'aa'
+        fill_in 'review_content', with: 'ab'
+
+        click_button '登録する'
+
+        visit books_path
+
+        book_shows = all('.book_show')
+
+        book_shows[0].click
+
+        expect(page).to have_selector 'p', text: 'kk'
+        expect(page).to have_selector 'h3', text: 'aa'
+        expect(page).to have_selector 'p', text: 'ab'
+      end
+
+      it 'レビューの編集ができること' do
+        click_link '本を検索'
+
+        fill_in 'keyword', with: '火車'
+
+        click_button '本の題名を検索'
+
+        book_shows = all('.book_show')
+
+        book_shows[0].click
+
+        click_link 'レビューを作成'
+
+        fill_in 'review_title', with: 'aa'
+        fill_in 'review_content', with: 'ab'
+
+        click_button '登録する'
+
+        click_link 'レビューを編集'
+
+        fill_in 'review_title', with: 'ac'
+        fill_in 'review_content', with: 'ad'
+
+        click_button '登録する'
+
+        expect(page).to have_selector 'p', text: 'kk'
+        expect(page).to have_selector 'h3', text: 'ac'
+        expect(page).to have_selector 'p', text: 'ad'
+      end
+      it 'ユーザーの書いたレビューが表示されていること' do
+        click_link '本を検索'
+
+        fill_in 'keyword', with: '火車'
+
+        click_button '本の題名を検索'
+
+        book_shows = all('.book_show')
+
+        book_shows[0].click
+
+        click_link 'レビューを作成'
+
+        fill_in 'review_title', with: 'aa'
+        fill_in 'review_content', with: 'ab'
+
+        click_button '登録する'
+
+        click_link 'ユーザー情報'
+
+        expect(page).to have_selector 'p', text: '火車'
+        expect(page).to have_selector 'p', text: 'kk'
+        expect(page).to have_selector 'h3', text: 'aa'
+        expect(page).to have_selector 'p', text: 'ab'
+      end
+      it 'レビュー作成後本一覧ページに本が表示されていること' do
+        click_link '本を検索'
+
+        fill_in 'keyword', with: '火車'
+
+        click_button '本の題名を検索'
+
+        book_shows = all('.book_show')
+
+        book_shows[0].click
+
+        click_link 'レビューを作成'
+
+        fill_in 'review_title', with: 'aa'
+        fill_in 'review_content', with: 'ab'
+
+        click_button '登録する'
+
+        visit books_path
+
+        expect(page).to have_selector 'p', text: '火車'
+      end
+    end
+  end
+  describe 'レビューの作成が失敗した時' do
+    before do
+      FactoryBot.create(:user)
+      visit new_user_session_path
+
+      fill_in 'user_email', with: 'kimu@kimu.com'
+      fill_in 'user_password', with: 'gaia0683'
+
+      click_button 'ログイン'
+    end
+    context 'レビューを新規作成する場合' do
+      it 'レビュー作成ページにリダイレクトしていること' do
+        click_link '本を検索'
+
+        fill_in 'keyword', with: '火車'
+
+        click_button '本の題名を検索'
+
+        book_shows = all('.book_show')
+
+        book_shows[0].click
+
+        click_link 'レビューを作成'
+
+        click_button '登録する'
+
+        expect(page).to have_content '内容を入力してください！'
       end
     end
   end
